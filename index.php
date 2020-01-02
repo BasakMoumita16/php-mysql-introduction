7d851ab2862fa67c24e41c798506efec484a7360
BasakMoumita16 <moumitaduttabasak@gmail.com>
2020-01-02 16:38:28 +0100

    database created

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="insert.php" method="Post">
+                id: <input type="text" name="id"><br>
+                first_name: <input type="text" name="fname"><br>
+                    last_ name: <input type="text" name="lname"><br>
+                    username: <input type="text" name="uname"><br>
+                    gender: <input type="text" name="gen"><br>
+                    linkedin: <input type="text" name="lin"><br>
+                    github: <input type="text" name="git"><br>
+                    email: <input type="text" name="emil"><br>
+                    preferred_language: <input type="text" name="ln"><br>     
+                    avatar: <input type="text" name="ava"><br>
+                    video: <input type="text" name="vid"><br>
+                quote: <input type="text" name="qut"><br>
+                quote_author: <input type="text" name="qutauthor"><br>
+                created_at: <input type="text" name="crt_at"><br><br>
+                <input type="submit" value="Submit">
+    </form>
+
+</body>
+</html>
